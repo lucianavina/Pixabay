193b18ffbc091765937bf62f072ecb29b104b3dc
Luciana Vina <lucianasvina@gmail.com>
2020-10-07 12:53:18 +0200

    search by page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
 
   const [search, saveSearch] = useState('')
   const [images, saveImages] = useState([])
+  const [actualPage, saveActualPage] = useState(1)
+  const [totalPages, saveTotalPages] = useState(1)
 
   useEffect(() => {
 
@@ -18,17 +20,46 @@ function App() {
 
       const imagesPerPage = 30
       const key = '18608981-cf112ac1e1811678f20cd9443'
-      const url = `https://pixabay.com/api/?key=${key}&q=${search}&per_page${imagesPerPage}`
+      const url = `https://pixabay.com/api/?key=${key}&q=${search}&per_page${imagesPerPage}&page=${actualPage}`
 
       const response = await fetch(url)
       const result = await response.json()
 
       saveImages(result.hits)
+
+      //get total pages
+
+      const getTotalPages = Math.ceil(result.totalHits/imagesPerPage)
+      saveTotalPages(getTotalPages)
+      
+      //moving above
+
+      const jumbotron = document.querySelector('.jumbotron')
+      jumbotron.scrollIntoView({ behavior: 'smooth' })
+      
     }
 
     callApi()
 
-  },[search])
+  }, [search, actualPage])
+  
+  const previousPage = () => {
+
+    const page = actualPage - 1
+
+    if(page === 0)return
+    
+    saveActualPage(page)
+  }
+
+  const nextPage = () => {
+
+    const page = actualPage + 1
+
+    if (page > totalPages) return
+
+    saveActualPage(page)
+  }
 
   return (
   
@@ -43,6 +74,29 @@ function App() {
         <ImagesList
           images={images}
         />
+
+        {(actualPage === 1) ? null : (
+          <button
+            type='button'
+            className='bbtn btn-info mr-1'
+            onClick = {previousPage}
+          > 
+              &laquo; Anterior
+          </button>
+        )}
+
+         {(actualPage === totalPages) ? null : (
+          <button
+            type='button'
+            className='bbtn btn-info mr-1'
+            onClick = {nextPage}>
+            Siguiente &raquo;
+          </button>
+        )}
+
+
+        
+        
       </div>
     </div>
     
--- src/Componenets/Image/index.js
+++ src/Componenets/Image/index.js
@@ -6,7 +6,20 @@ const Image = ({ image }) => {
     return ( 
         <div className='col-12 col-sm-6 col-md-4 col-lg-3 mb-4'>
             <div className='card'>
-                <img src={previewURL} alt={tags} className='carg-img-top'/>
+                <img src={previewURL} alt={tags} className='carg-img-top' />
+                
+                <div className='card-body'>
+                    <p className='card-text'>{likes} Me Gusta</p>
+                    <p className='card-text'>{views} Vistas</p>
+                </div>
+                <div className='card-footer'>
+                    <a
+                        href={largeImageURL}
+                        target='_blank'
+                        rel='nooperner noreferrer'
+                        className='btn btn-primary btn-block'
+                    >Ver Imágen</a>
+                </div>
             </div>
     </div> );
 }
